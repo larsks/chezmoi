f6317a9f7db4d2cee4efe4ccaff48b783c740dc0
Lars Kellogg-Stedman <lars@oddbit.com>
2023-02-16 09:40:00 -0500

    Make it possible to disable kubeconfig processing

--- exact_dot_bashrc.d/kubectl.sh
+++ exact_dot_bashrc.d/kubectl.sh
@@ -9,6 +9,8 @@ complete -o default -F __start_kubectl k
 _rebuild_kubeconfig() {
 	local _kubeconfigs
 
+	[[ -f $HOME/.kube/rebuild-kubeconfig/disable ]] && return
+
 	# shellcheck disable=SC2086
 	readarray -t _kubeconfigs < <(find $HOME/.kube/configs -name default -prune -o -type f -print)
 	printf -v KUBECONFIG "%s:" "$HOME/.kube/configs/default" "${_kubeconfigs[@]}"
