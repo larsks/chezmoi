48da49c3c811412e4db5a6830d45d17df3416b35
Lars Kellogg-Stedman <lars@oddbit.com>
2023-03-16 11:08:22 -0400

    check for aduino cli before sourcing completion

--- exact_dot_bashrc.d/arduino-cli.sh
+++ exact_dot_bashrc.d/arduino-cli.sh
@@ -1,4 +1,6 @@
 #!/bin/bash
 
-# shellcheck disable=SC1090
-. <(arduino-cli completion bash)
+if type -t arduino-cli > /dev/null 2>&1; then
+	# shellcheck disable=SC1090
+	. <(arduino-cli completion bash)
+fi
